d8a47dda1be67346e10dffa886df5239fc3e7518
supla <supla@supla-iotsdk.localhost>
2016-03-17 12:46:21 -0400

    Pierwsza proba dodania do kodu obslugi sciemniacza.

---
+++ ESP8266_IOT/user/supla_triac_dimmer.c
@@ -0,0 +1,21 @@
+#include <os_type.h>
+#include <osapi.h>
+#include <gpio.h>
+#include <eagle_soc.h>
+#include <ets_sys.h>
+
+#include "supla_ds18b20.h"
+#include "supla-dev/log.h"
+
+
+
+void ICACHE_FLASH_ATTR supla_triac_dimmer_init(void)
+{
+
+      
+
+}
+
+
+
+
---
+++ ESP8266_IOT/user/supla_triac_dimmer.h
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : supla_triac_dimmer.h
+ Author      : Andrzej Jurkowski biuro@easyel.pl [EASYEL]
+ Version     : 0.1
+ Copyright   : 2016 GPLv2
+ ============================================================================
+*/
+
+#ifndef SUPLA_TRIAC_DIMMER_H_
+#define SUPLA_TRIAC_DIMMER_H_
+
+#include "supla_esp.h"
+
+void ICACHE_FLASH_ATTR supla_triac_dimmer_init(void);
+
+#endif
